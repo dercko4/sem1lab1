57722193a371fe455773037706aa9a5778c10f4e
dercko4 <dercko4@mail.ru>
2024-10-03 12:12:15 +0300

    Связь

--- models/model.js
+++ models/model.js
@@ -18,24 +18,11 @@ const Manufacturer = sequelize.define('manufacturers', {
     name_of_organization: {type: DataTypes.STRING}
 })
 
-const Product = sequelize.define('products',{
-    id_product: {type: DataTypes.UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
-    id_manufacturer: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4, references: {model: Manufacturer, key: 'id_manufacturer'}},
-    id_category: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
-    id_warehouse: {type: DataTypes.UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
-    product_name: {type: DataTypes.STRING},
-    expiration: {type: DataTypes.DATE},
-    cost: {type: DataTypes.FLOAT},
-    description: {type: DataTypes.STRING},
-    quantity_in_warehouse: {type: DataTypes.FLOAT},
-})
-
 const Warehouse = sequelize.define('warehouses', {
     id_warehouse: {type: DataTypes.UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
     address: {type: DataTypes.STRING},
     capacity: {type: DataTypes.INTEGER}
 })
-
 
 const Category = sequelize.define('categories', {
     id_category: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
@@ -43,22 +30,39 @@ const Category = sequelize.define('categories', {
     is_product_weight_category: {type: DataTypes.BOOLEAN}
 })
 
+
+const Product = sequelize.define('products',{
+    id_product: {type: DataTypes.UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
+    id_manufacturer: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4, references: {model: Manufacturer, key: 'id_manufacturer'}},
+    id_category: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4, references: {model: Category, key: 'id_category'}},
+    id_warehouse: {type: DataTypes.UUID, primaryKey: true, defualtValue: sequelize.UUIDV4, references: {model: Warehouse, key: 'id_warehouse'}},
+    product_name: {type: DataTypes.STRING},
+    expiration: {type: DataTypes.DATE},
+    cost: {type: DataTypes.FLOAT},
+    description: {type: DataTypes.STRING},
+    quantity_in_warehouse: {type: DataTypes.FLOAT},
+})
+
+
 const Basket_Product = sequelize.define('basket_products', {
     id_basket: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
-    id_product: {type: DataTypes.UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
+    id_product: {type: DataTypes.UUID, primaryKey: true, defualtValue: sequelize.UUIDV4, references: {model: Product, key: 'id_product'}},
     quantity_to_buy: {type: DataTypes.INTEGER}
 })
 
 const Order = sequelize.define('orders', {
     id_order: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
-    id_basket: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
-    id_user: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4},
+    id_basket: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4, references: {model: Basket_Product, key: 'id_basket'}},
+    id_user: {type: DataTypes. UUID, primaryKey: true, defualtValue: sequelize.UUIDV4, references: {model: Manufacturer, key: 'id_user'}},
     full_cost: {type: DataTypes.FLOAT},
     status: {type: DataTypes.STRING}
 })
 
 User.hasMany(Order, {
-    foreignKey: ''
+    foreignKey: {
+        model: Order,
+        name: 'id_order'
+    }
 })
 Order.belongsTo(User)
 
