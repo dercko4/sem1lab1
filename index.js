f25ed25af58b4fefa40814b2d4fb5aab5f9f34c7
Мухторов Рустам Самариддинович <r.mukhtorov@mkit.local>
2025-03-20 10:14:33 +0300

    feat: add cache

--- index.js
+++ index.js
@@ -4,30 +4,51 @@ const sequelize = require("./database")
 const models = require('./models/model')
 const cors = require('cors')
 const routes_createDB = require('./routes/createDB/index')
-
+const crypto = require('crypto');
 
 const http = require('http')
-const HOST=process.env.HOST
+const HOST = process.env.HOST
 const PORT = process.env.PORT
 const app = express()
 const server = http.createServer(app)
 
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    next();
-  });
+
 app.use(cors())
 app.use(express.json())
+
+async function generateETag(data) {
+    return await crypto.createHash('md5').update(data).digest('hex');
+}
+
+
+app.use((req, res, next) => {
+    const originalSend = res.send;
+    res.send = async function (body) {
+        const generatedEtag = await generateETag(body);
+        console.log(generatedEtag)
+        const lastModified = new Date().toUTCString();
+        res.setHeader('ETag', generatedEtag);
+        res.setHeader('Cache-Control', 'public, max-age=86400');
+        if (req.headers['if-none-match'] === generatedEtag || req.headers['if-modified-since'] === lastModified) {
+            return res.status(304).end();
+        }
+        originalSend.call(this, body);
+    }
+    next()
+});
+
+
+
 app.use(routes_createDB)
 
 const start = async () => {
-    try{
+    try {
         await sequelize.authenticate()
         await sequelize.sync()
         server.listen(PORT, HOST, () => console.log(`Server start on ${HOST}:${PORT}`))
     }
-    catch(e){
+    catch (e) {
         console.log(e)
     }
 }
